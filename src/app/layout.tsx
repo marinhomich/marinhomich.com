1a23d00547a2a3c26d15d6ad4721ed0a54c2d246
Michel Marinho <michel.marinho04@outlook.com>
2023-07-17 14:02:18 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,25 @@
 import "@/styles/globals.css";
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Dashboard - Michel Marinho',
-  description: 'Dashboard - Michel Marinho',
-}
+  title: "Dashboard - Michel Marinho",
+  description: "Dashboard - Michel Marinho",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-      <Providers>
-        {children}
-        </Providers>
-        </body>
+      <body className={"min-h-screen font-sans antialiased"}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
