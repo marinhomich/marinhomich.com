c759f7518d972201e0da0ac5f029f66b3cfffe4e
Michel Marinho <michel.marinho04@outlook.com>
2023-08-11 07:54:36 -0400

    refactor: sidebar component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,15 @@
-import Nav from "@/components/Nav";
+import Sidebar from "@/components/Sidebar";
 import Profile from "@/components/profile";
 import { ReactNode, Suspense } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Nav>
+      <Sidebar>
         <Suspense fallback={<div>Loading...</div>}>
           <Profile />
         </Suspense>
-      </Nav>
+      </Sidebar>
       <div className="min-h-screen dark:bg-black sm:pl-60">{children}</div>
     </div>
   );
