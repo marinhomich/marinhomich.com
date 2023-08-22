3449973e5d8cab2faaa32228104cff3fba66ae31
Michel Marinho <michel.marinho04@outlook.com>
2023-08-22 16:42:46 -0400

    update

--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -8,6 +8,7 @@ export default function LogoutButton() {
     <button
       onClick={() => signOut()}
       className="rounded-lg p-1.5 transition-all duration-150 ease-in-outtext-white hover:bg-secondary/80 active:bg-secondary"
+      aria-label="Logout"
     >
       <LogOut width={18} />
     </button>
