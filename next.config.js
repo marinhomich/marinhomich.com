6380815f9e7a0481e1dbd823985b3582c276d9fb
Michel Marinho <michel.marinho04@outlook.com>
2023-09-18 12:22:43 -0400

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,22 @@ const nextConfig = {
   },
   reactStrictMode: false,
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "dashboard.marinhomich.dev",
+          },
+        ],
+        destination: "https://app.marinhomich.dev",
+        permanent: true,
+        statusCode: 301,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
