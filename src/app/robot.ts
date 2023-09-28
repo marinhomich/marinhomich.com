355c9e83881af2ced2de7d5d996b625730f6b1e5
Michel Marinho <michel.marinho04@outlook.com>
2023-09-28 09:46:15 -0400

    update sitemap

---
+++ src/app/robot.ts
@@ -0,0 +1,19 @@
+import { type MetadataRoute } from "next"
+import { headers } from "next/headers"
+
+import { isHomeHostname } from "@/lib/constants"
+
+export default function robots(): MetadataRoute.Robots {
+  const headersList = headers()
+  let domain = headersList.get("host") as string
+  if (isHomeHostname(domain)) domain = "marinhomich.dev"
+
+  return {
+    rules: {
+      userAgent: "*",
+      disallow: "/api/",
+      allow: "/api/og/",
+    },
+    sitemap: `https://${domain}/sitemap.xml`,
+  }
+}
