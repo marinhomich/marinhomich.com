0d9a2cd0aa9541f7b69d415d78e127ac3b76f89d
Michel Marinho <michel.marinho04@outlook.com>
2023-09-28 13:35:35 -0400

    fix: middleware

--- src/lib/middleware/app.ts
+++ src/lib/middleware/app.ts
@@ -1,12 +1,13 @@
-import { type NextRequest, NextResponse } from "next/server";
-import { parse } from "./utils";
-import { getToken } from "next-auth/jwt";
+import { NextResponse, type NextRequest } from "next/server"
+import { getToken } from "next-auth/jwt"
 
-export default async function AppMiddleware(req: NextRequest){
-  const { path } = parse(req);
+import { parse } from "./utils"
+
+export default async function AppMiddleware(req: NextRequest) {
+  const { path } = parse(req)
 
   const session = await getToken({ req })
-  if (!session && path !== "/login") {
+  if (!session && path !== "/login" && path !== "/signup") {
     return NextResponse.redirect(new URL("/login", req.url))
   } else if (session && path == "/login") {
     return NextResponse.redirect(new URL("/", req.url))
