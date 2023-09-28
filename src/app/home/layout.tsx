4a76802826540b84a65b8591cdb03a95756ce731
Michel Marinho <michel.marinho04@outlook.com>
2023-09-28 09:22:09 -0400

    added robots

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -42,7 +42,6 @@ export const metadata: Metadata = {
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-  manifest: `${siteConfig.url}/site.webmanifest`,
 }
 export default function RootLayout({
   children,
