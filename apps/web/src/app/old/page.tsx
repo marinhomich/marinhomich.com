4a796fe3e1ff893c5486afa9561a7803e24d8b2c
Michel Marinho <michel.marinho04@outlook.com>
2023-11-10 08:53:22 -0400

    update

--- apps/web/src/app/old/page.tsx
+++ apps/web/src/app/old/page.tsx
@@ -2,23 +2,21 @@ import Image from "next/image"
 import { notFound } from "next/navigation"
 import { list } from "@vercel/blob"
 
-import { getSiteData } from "@/lib/fetchers"
-
 export default async function CustomDomainPage({
   params,
 }: {
   params: { domain: string }
 }) {
   const domain = decodeURIComponent(params.domain)
-  const data = await getSiteData(domain)
+  // const data = await getSiteData(domain)
   const { blobs } = await list()
-  if (!data) {
-    notFound()
-  }
+  // if (!data) {
+  //   notFound()
+  // }
 
   return (
     <div>
-      <Image src={data.logo || ""} width={200} height={200} alt="logo" />
+      {/* <Image src={data.logo || ""} width={200} height={200} alt="logo" /> */}
       <p>{domain}</p>
     </div>
   )
