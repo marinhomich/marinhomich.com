a34208a9e2fbff0d3d5a5c61f7bf55a217e23ba9
Michel Marinho <marinhomich@gmail.com>
2024-01-19 13:15:07 -0400

    updated to React Email 2.0

--- apps/web/src/emails/contact-email.tsx
+++ apps/web/src/emails/contact-email.tsx
@@ -1,4 +1,4 @@
-import { Html } from "@react-email/html"
+import { Body, Container, Hr, Html, Text } from "@react-email/components"
 
 interface Props {
   name: string
@@ -11,17 +11,46 @@ export default function ContactEmail(props: Props) {
 
   return (
     <Html>
-      <ul>
-        <li>
-          <strong>Name:</strong> {name}
-        </li>
-        <li>
-          <strong>Email:</strong> {email}
-        </li>
-        <li>
-          <strong>Message:</strong> {message}
-        </li>
-      </ul>
+      <Body style={main}>
+        <Container style={container}>
+          <ul>
+            <li>
+              <strong>Name:</strong> {name}
+            </li>
+            <li>
+              <strong>Email:</strong> {email}
+            </li>
+            <li>
+              <strong>Message:</strong> {message}
+            </li>
+          </ul>
+
+          <Hr style={hr} />
+          <Text style={footer}>marinhomich</Text>
+          <Text style={footer}>React Email 2.0</Text>
+        </Container>
+      </Body>
     </Html>
   )
+}
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+}
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+}
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+}
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
 }
