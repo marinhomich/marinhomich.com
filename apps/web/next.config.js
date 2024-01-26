7acf12ba80374c2abbedbf6db39fe966f9a8148c
Michel Marinho <marinhomich@gmail.com>
2024-01-26 15:38:47 -0400

    feat: utils package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@ const { withContentlayer } = require("next-contentlayer")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@marinhomich/ui"],
   experimental: {
     serverActions: {
       allowedOrigins: [
